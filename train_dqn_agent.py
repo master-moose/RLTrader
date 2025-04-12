fa3c60e2c57171e4e23f1274097e5a94e068f278
master-moose <jai.garrovillo@gmail.com>
2025-04-12 19:40:15 +0200

    Fix SAC model parameters by properly nesting net_arch under policy_kwargs

--- train_dqn_agent.py
+++ train_dqn_agent.py
@@ -623,14 +623,16 @@ def train_with_finrl(args, market_data, device):
                 'learning_rate': args.learning_rate,
                 'device': device,
                 'action_noise': 'normal' if action_noise else None,
-                'net_arch': {
-                    'pi': net_arch,  # Policy network
-                    'qf': net_arch   # Q-function network
+                'policy_kwargs': {
+                    'net_arch': {
+                        'pi': net_arch,  # Policy network
+                        'qf': net_arch   # Q-function network
+                    }
                 }
             }
         else:
             # For other models, simpler net_arch specification
-            model_params['net_arch'] = net_arch
+            model_params['policy_kwargs'] = {'net_arch': net_arch}
 
     # Create and train the model
     try:
