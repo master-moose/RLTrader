16147c1acf4683720b732c0cbbba41352206cbdd
master-moose <jai.garrovillo@gmail.com>
2025-04-15 20:37:53 +0200

    Remove checkpoint_at_end for Ray 2.5.1 compatibility

--- rl_agent/tune/run_tune_sweep.py
+++ rl_agent/tune/run_tune_sweep.py
@@ -279,7 +279,6 @@ def run_tune_experiment(args):
         local_dir=args.local_dir,
         name=args.exp_name,
         checkpoint_freq=2,
-        checkpoint_at_end=True,
         keep_checkpoints_num=2,
         metric="eval/mean_reward",
         mode="max",
