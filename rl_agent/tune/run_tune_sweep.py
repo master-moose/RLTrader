209ad48092cc9358359af24d804d65800e84c9d8
master-moose <jai.garrovillo@gmail.com>
2025-04-26 23:41:56 +0200

    refactor: Revert to using scaled OHLC features in default config

--- rl_agent/tune/run_tune_sweep.py
+++ rl_agent/tune/run_tune_sweep.py
@@ -72,7 +72,7 @@ DEFAULT_CONFIG = {
     # Features - selected important features
     # Split into multiple lines for readability
     "features": (
-        "open,high,low,close,volume_scaled,"  # Raw OHLC, Scaled Volume (1m)
+        "open_scaled,high_scaled,low_scaled,close_scaled,volume_scaled,"  # Scaled OHLCV (1m)
         "sma_7_scaled,sma_25_scaled,sma_99_scaled,ema_9_scaled,"
         "ema_21_scaled,rsi_14_scaled,"
         "volume_scaled_4h,ema_9_scaled_4h,ema_21_scaled_4h,"  # 4h
